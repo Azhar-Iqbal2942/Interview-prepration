0798130d7158aef14932dfd8aa71b719eed342df
Azhar Iqbal <azhariqbal2942@gmail.com>
2020-09-22 09:01:33 -0700

    Implementation completed

--- Data Structure/Non Linear/avl_tree.py
+++ Data Structure/Non Linear/avl_tree.py
@@ -29,19 +29,72 @@ class AVLTree:
         else:
             root.left_child = self.__insert(root.left_child, value)
 
-        root.height = max(self.__height(root.left_child),
-                          self.__height(root.right_child)) + 1
-        print(root.height)
+        self.__set_height(root)
+        return self.__auto_balancer(root)
+
+    def __auto_balancer(self, root):
+        if self.__is_left_heavy(root):
+            if self.__balance_factor(root.left_child) < 0:
+                root.left_child = self.__rotate_left(root.left_child)
+            return self.__rotate_right(root)
+
+        if self.__is_right_heavy(root):
+            if self.__balance_factor(root.right_child) > 0:
+                root.right_child = self.__rotate_right(root.right_child)
+            return self.__rotate_left(root)
 
         return root
+
+    def __rotate_left(self, root):
+        new_root = root.right_child
+
+        root.right_child = new_root.left_child
+        new_root.left_child = root
+
+        # now update heights
+        self.__set_height(root)
+        self.__set_height(new_root)
+
+        return new_root
+
+    def __rotate_right(self, root):
+        new_root = root.left_child
+
+        root.left_child = new_root.right_child
+        new_root.right_child = root
+
+        # update heights
+        self.__set_height(root)
+        self.__set_height(new_root)
+
+        return new_root
+
+    def __set_height(self, node):
+        node.height = max(self.__height(node.left_child),
+                          self.__height(node.right_child)) + 1
+
+    def __is_left_heavy(self, node):
+        return self.__balance_factor(node) > 1
+
+    def __is_right_heavy(self, node):
+        return self.__balance_factor(node) < -1
+
+    def __balance_factor(self, node):
+        return 0 if node is None else self.__height(node.left_child) - self.__height(node.right_child)
 
     def __height(self, root):
         return -1 if root == None else root.height
 
 
 tree = AVLTree()
+tree.insert(20)
 tree.insert(10)
-tree.insert(8)
+tree.insert(30)
+tree.insert(15)
+tree.insert(13)
+
+
+print(tree.root.left_child)
 
 
 # print(tree.height())
